926e9e4d300548f622225fb95825e5af8acca74f
1Fenix7 <userpk66@gmail.com>
2024-02-03 11:13:31 +0300

    feat: :sparkles: add simple auth by clerk service

---
+++ app/(platform)/layout.tsx
@@ -0,0 +1,5 @@
+import { ClerkProvider } from '@clerk/nextjs';
+
+export default function PlatformLayout({ children }: { children: React.ReactNode }) {
+	return <ClerkProvider>{children}</ClerkProvider>;
+}
