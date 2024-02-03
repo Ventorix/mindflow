45fce466f8615714a2a8be8c6a9667673c4963da
1Fenix7 <userpk66@gmail.com>
2024-02-03 13:01:33 +0300

    feat: :sparkles: add organization page

---
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -0,0 +1,55 @@
+import { Plus } from 'lucide-react';
+import { OrganizationSwitcher, UserButton } from '@clerk/nextjs';
+
+import { Button } from '@/components/ui/button';
+import Logo from '@/components/ui/logo';
+
+export default function Navbar() {
+	return (
+		<nav className='fixed z-50 top-0 px-4 w-full h-14 border-b shadow-sm bg-white flex items-center'>
+			<div className='flex items-center gap-x-4'>
+				<div className='hidden md:flex'>
+					<Logo />
+				</div>
+				<Button
+					variant='primary'
+					size='sm'
+					className='rounded-sm hidden md:block h-auto py-1.5 px-2'>
+					Create
+				</Button>
+				<Button variant='primary' size='sm' className='rounded-sm block md:hidden'>
+					<Plus className='h-4 w-4' />
+				</Button>
+			</div>
+
+			<div className='ml-auto flex items-center gap-x-2'>
+				<OrganizationSwitcher
+					hidePersonal
+					afterCreateOrganizationUrl={'/organization/:id'}
+					afterLeaveOrganizationUrl='/select-org'
+					afterSelectOrganizationUrl='organization/:id'
+					appearance={{
+						elements: {
+							rootBox: {
+								display: 'flex',
+								justifyContent: 'center',
+								alignItems: 'center',
+							},
+						},
+					}}
+				/>
+				<UserButton
+					afterSignOutUrl='/'
+					appearance={{
+						elements: {
+							avatarBox: {
+								height: 30,
+								width: 30,
+							},
+						},
+					}}
+				/>
+			</div>
+		</nav>
+	);
+}
