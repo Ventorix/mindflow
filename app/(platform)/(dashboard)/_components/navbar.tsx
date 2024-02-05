b5f5b7334c8c673f1d766ad7ad131f122b955e78
1Fenix7 <userpk66@gmail.com>
2024-02-05 08:49:18 +0300

    feat: :sparkles: add mobile sidebar

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -4,9 +4,12 @@ import { OrganizationSwitcher, UserButton } from '@clerk/nextjs';
 import { Button } from '@/components/ui/button';
 import Logo from '@/components/ui/logo';
 
+import MobileSidebar from './mobile-sidebar';
+
 export default function Navbar() {
 	return (
 		<nav className='fixed z-50 top-0 px-4 w-full h-14 border-b shadow-sm bg-white flex items-center'>
+			<MobileSidebar />
 			<div className='flex items-center gap-x-4'>
 				<div className='hidden md:flex'>
 					<Logo />
