074d5480705b17c10d81365e1f30d59ea4f8c408
1Fenix7 <userpk66@gmail.com>
2024-02-05 11:08:48 +0300

    feat: :sparkles: add skeleton to sidebar

--- app/(platform)/(dashboard)/_components/nav-item.tsx
+++ app/(platform)/(dashboard)/_components/nav-item.tsx
@@ -7,6 +7,7 @@ import { Activity, CreditCard, Layout, Settings } from 'lucide-react';
 import { cn } from '@/lib/utils';
 import { AccordionContent, AccordionItem, AccordionTrigger } from '@/components/ui/accordion';
 import { Button } from '@/components/ui/button';
+import { Skeleton } from '@/components/ui/skeleton';
 
 export type Organization = {
 	id: string;
@@ -92,3 +93,14 @@ export function NavItem({ isExpanded, isActive, organization, onExpand }: NavIte
 		</AccordionItem>
 	);
 }
+
+NavItem.Skeleton = function SkeletonNavItem() {
+	return (
+		<div className='flex items-center gap-x-2'>
+			<div className='w-10 h-10 relative shrink-0'>
+				<Skeleton className='h-full w-full absolute' />
+			</div>
+			<Skeleton className='h-10 w-full' />
+		</div>
+	);
+};
