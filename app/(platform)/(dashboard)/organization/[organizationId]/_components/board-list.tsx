e7cec14eb26c04cb6bce3d9ad2719095c5d61782
1Fenix7 <userpk66@gmail.com>
2024-03-03 20:38:59 +0300

    feat: :sparkles: add a popover form to create a new board

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -1,6 +1,7 @@
 import { HelpCircle, User2 } from 'lucide-react';
 
 import { Hint } from '@/components/hint';
+import { FormPopover } from '@/components/form/form-popover';
 
 export const BoardList = () => {
 	return (
@@ -11,17 +12,19 @@ export const BoardList = () => {
 			</div>
 
 			<div className='grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4'>
-				<div
-					role='button'
-					className='aspect-video relative h-full w-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition'>
-					<p className='text-sm'>Create new board</p>
-					<span className='text-xs'>5 remaining</span>
-					<Hint
-						sideOffset={40}
-						description={`Free Workspaces can up to 5 open boards. For unlimited boards, please, upgrade this workspace.`}>
-						<HelpCircle className='absolute bottom-2 right-2 h-[14px] w-[14px]' />
-					</Hint>
-				</div>
+				<FormPopover sideOffset={10} side='right'>
+					<div
+						role='button'
+						className='aspect-video relative h-full w-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition'>
+						<p className='text-sm'>Create new board</p>
+						<span className='text-xs'>5 remaining</span>
+						<Hint
+							sideOffset={40}
+							description={`Free Workspaces can up to 5 open boards. For unlimited boards, please, upgrade this workspace.`}>
+							<HelpCircle className='absolute bottom-2 right-2 h-[14px] w-[14px]' />
+						</Hint>
+					</div>
+				</FormPopover>
 			</div>
 		</div>
 	);
---
+++ components/form/form-popover.tsx
@@ -0,0 +1,62 @@
+'use client';
+
+import { XIcon } from 'lucide-react';
+import { Button } from '../ui/button';
+import { Popover, PopoverClose, PopoverContent, PopoverTrigger } from '../ui/popover';
+import { FormInput } from './form-input';
+import { FormSubmit } from './form-submit';
+import { useAction } from '@/hooks/use-action';
+import { createBoard } from '@/actions/create-board';
+
+interface FormPopoverProps {
+	children: React.ReactNode;
+	side?: 'left' | 'right' | 'top' | 'bottom';
+	align?: 'start' | 'center' | 'end';
+	sideOffset?: number;
+}
+
+export const FormPopover = ({
+	children,
+	side = 'bottom',
+	align,
+	sideOffset = 0,
+}: FormPopoverProps) => {
+	const { execute, fieldErrors } = useAction(createBoard, {
+		onSuccess(data) {
+			console.log({ data });
+		},
+		onError(error) {
+			console.log(error);
+		},
+	});
+
+	const onSubmit = (formData: FormData) => {
+		const title = formData.get('title') as string;
+
+		execute({ title });
+	};
+
+	return (
+		<div>
+			<Popover>
+				<PopoverTrigger asChild>{children}</PopoverTrigger>
+				<PopoverContent align={align} className='w-80 pt-3' side={side} sideOffset={sideOffset}>
+					<div className='text-sm font-medium text-center text-neutral-600 pb-4'>Create board</div>
+					<PopoverClose asChild>
+						<Button
+							className='h-auto w-auto p-2 absolute top-2 right-2 text-neutral-600'
+							variant={'ghost'}>
+							<XIcon className='h-4 w-4' />
+						</Button>
+					</PopoverClose>
+					<form action={onSubmit} className='space-y-4'>
+						<div className='space-y-4'>
+							<FormInput id='title' label='Board title' type='text' errors={fieldErrors} />
+						</div>
+						<FormSubmit className='w-full'>Create</FormSubmit>
+					</form>
+				</PopoverContent>
+			</Popover>
+		</div>
+	);
+};
