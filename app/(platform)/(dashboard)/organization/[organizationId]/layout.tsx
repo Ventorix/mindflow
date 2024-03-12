8ee314b49a61f78a82d1b63a6f845adfd164d056
1Fenix7 <userpk66@gmail.com>
2024-03-12 22:08:42 +0300

    feat: :sparkles: add appropriate tab title if using organization

--- app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,5 +1,14 @@
+import { auth } from '@clerk/nextjs';
 import { OrgControl } from './_components/org-control';
+import { startCase } from 'lodash';
 
+export async function generateMetadata() {
+	const { orgSlug } = auth();
+
+	return {
+		title: startCase(orgSlug || 'organization'),
+	};
+}
 export default function OrganizationIdLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<>
