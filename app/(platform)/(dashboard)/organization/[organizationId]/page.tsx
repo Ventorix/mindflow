b466eccd2ce4e3a788875d65980ab323ed8dc1f5
1Fenix7 <userpk66@gmail.com>
2024-03-12 21:58:17 +0300

    feat: :sparkles: add board list and skeleton for it

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,4 +1,7 @@
+import { Suspense } from 'react';
+
 import { Separator } from '@/components/ui/separator';
+
 import { Info } from './_components/info';
 import { BoardList } from './_components/board-list';
 
@@ -8,7 +11,9 @@ export default async function OrganizationIdPage() {
 			<Info />
 			<Separator className='my-4' />
 			<div className='px-2 md:px-4'>
-				<BoardList />
+				<Suspense fallback={<BoardList.Skeleton />}>
+					<BoardList />
+				</Suspense>
 			</div>
 		</div>
 	);
