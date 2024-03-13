ee838fda7589eb9da08b9a2c00541d6c62259cd1
1Fenix7 <userpk66@gmail.com>
2024-03-14 01:51:07 +0300

    feat: :sparkles: add feature to delete board

--- actions/delete-board/index.ts
+++ actions/delete-board/index.ts
@@ -2,11 +2,12 @@
 
 import { auth } from '@clerk/nextjs';
 import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
 
 import { db } from '@/lib/db';
 import { createSafeAction } from '@/lib/create-safe-action';
 
-import { UpdateBoard } from './schema';
+import { DeleteBoard } from './schema';
 import { InputType, ReturnType } from './types';
 
 const handler = async (data: InputType): Promise<ReturnType> => {
@@ -18,27 +19,24 @@ const handler = async (data: InputType): Promise<ReturnType> => {
 		};
 	}
 
-	const { title, id } = data;
+	const { id } = data;
 	let board;
 
 	try {
-		board = await db.board.update({
+		board = await db.board.delete({
 			where: {
 				id,
 				orgId,
 			},
-			data: {
-				title,
-			},
 		});
 	} catch (error) {
 		return {
-			error: 'Failed to update',
+			error: 'Failed to delete',
 		};
 	}
 
-	revalidatePath(`/board/${id}`);
-	return { data: board };
+	revalidatePath(`/organization/${orgId}`);
+	redirect(`/organization/${orgId}`);
 };
 
-export const updateBoard = createSafeAction(UpdateBoard, handler);
+export const deleteBoard = createSafeAction(DeleteBoard, handler);
