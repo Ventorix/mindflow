b4c4e54789199d9f70f0afc2658e34abe4bd5fab
1Fenix7 <userpk66@gmail.com>
2024-03-14 01:28:10 +0300

    feat: :sparkles: add feautre to change board title

---
+++ app/(platform)/(dashboard)/board/[boardId]/_components/board-options.tsx
@@ -0,0 +1,8 @@
+'use client';
+
+interface BoardOptionsProps {
+	id: string;
+}
+export const BoardOptions = ({ id }: BoardOptionsProps) => {
+	return <div>Options</div>;
+};
--- app/(platform)/(dashboard)/board/[boardId]/layout.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/layout.tsx
@@ -1,6 +1,7 @@
 import { db } from '@/lib/db';
 import { auth } from '@clerk/nextjs';
 import { notFound, redirect } from 'next/navigation';
+import { BoardNavbar } from './_components/board-navbar';
 
 export async function generateMetadata({ params }: { params: { boardId: string } }) {
 	const { orgId } = auth();
@@ -50,6 +51,8 @@ const BoardIdLayout = async ({
 		<div
 			className='relative h-full bg-no-repeat bg-cover bg-center'
 			style={{ backgroundImage: `url(${board.imageFullUrl})` }}>
+			<BoardNavbar data={board} />
+			<div className='absolute inset-0 bg-black/10' />
 			<main className='relative pt-28 h-full '>{children}</main>
 		</div>
 	);
