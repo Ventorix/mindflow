22db1d74fd453e1eda01f275127b6f2a5e6ed810
1Fenix7 <userpk66@gmail.com>
2024-03-14 18:42:13 +0300

    fix: :bug: fix error that occurs when a form submits incorrect data

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-form.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-form.tsx
@@ -78,7 +78,7 @@ export const ListForm = () => {
 						className='text-sm px-2 py-1 h-7 font-medium border-transparent hover:border-input focus:border-input transition'
 						placeholder='Enter list title...'
 					/>
-					<input hidden value={params.id} name='boardId' />
+					<input hidden value={params.boardId} name='boardId' />
 					<div className='flex items-center gap-x-1'>
 						<FormSubmit>Add list</FormSubmit>
 						<Button onClick={disableEditing} size={'sm'} variant={'ghost'}>
