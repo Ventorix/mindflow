bc8ae02c62da45754cda05b7a625db99e5f94770
1Fenix7 <userpk66@gmail.com>
2024-03-15 03:03:06 +0300

    feat: :sparkles: add card form

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
@@ -3,6 +3,8 @@
 import { ListWithCards } from '@/types';
 
 import { ListHeader } from './list-header';
+import { ElementRef, useRef, useState } from 'react';
+import { CardForm } from './card-form';
 
 interface ListItemProps {
 	index: number;
@@ -10,10 +12,31 @@ interface ListItemProps {
 }
 
 export const ListItem = ({ index, data }: ListItemProps) => {
+	const textAreaRef = useRef<ElementRef<'textarea'>>(null);
+	const [isEditing, setIsEditing] = useState(false);
+
+	const enableEditing = () => {
+		setIsEditing(true);
+		setTimeout(() => {
+			textAreaRef.current?.focus();
+		});
+	};
+
+	const disableEditing = () => {
+		setIsEditing(false);
+	};
+
 	return (
 		<li className='shrink-0 h-full w-[272px] select-none'>
 			<div className='w-full rounded-md bg-[#f1f2f4] shadow-md pb-2'>
-				<ListHeader data={data} />
+				<ListHeader onAddCard={enableEditing} data={data} />
+				<CardForm
+					listId={data.id}
+					ref={textAreaRef}
+					isEditing={isEditing}
+					enableEditing={enableEditing}
+					disableEditing={disableEditing}
+				/>
 			</div>
 		</li>
 	);
